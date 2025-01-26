472ba9976ad3b2bae49fa17aee395499d0256598
EmmaNguyen0410 <emmang0410@gmail.com>
2025-01-26 11:59:24 +1100

    add annotation

--- src/main/java/com/emmang/auth/exception/CustomExceptionHandler.java
+++ src/main/java/com/emmang/auth/exception/CustomExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.emmang.auth.exception;
 
-import com.emmang.auth.constant.ExceptionMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
